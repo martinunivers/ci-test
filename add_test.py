8d5e806642d6ef6e4f0054e4e42cfb660ffa1055
Martin <32496582+martinunivers@users.noreply.github.com>
2020-02-28 15:23:56 -0500

    Update add_test.py

--- add_test.py
+++ add_test.py
@@ -1,11 +1,5 @@
-import unittest
+import pytest
 from add import add
 
-class Test(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 1), 3, "Should be 3")
-        print(add(2, 1))
-    
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add():
+    assert add(1, 2) == 3
